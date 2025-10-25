13ed420152338c11b364f59f8f8188a4cefdc056
josivan98 <josivanviana16@gmail.com>
2025-10-25 19:40:14 +0000

    calculado per capita e densidade ppopul

--- CARTASDOJOGO_1.C
+++ CARTASDOJOGO_1.C
@@ -5,18 +5,25 @@ int main(){
     char estado = 'A'; 
     char codigo[5] = "A01";
     char cidade[20] = "Fortaleza";
-    int populaçao = 2578483;
+    float populaçao = 2578483;
     float area = 313.8;
-    float pib = 73.4;
+    float pib = 7.34;
     int pontos_turisticos = 66;
+    float dens_popu = 0.82;
+    float per_capita = 28.4;
 
     char estado = 'B'; 
     char codigo[5] = "B01";
     char cidade[20] = "bahia";
-    int populaçao = 14.8;
-    float area = 567.295;
-    float pib = 131.7;
+    float populaçao = 14800000;
+    float area = 567295;
+    float pib = 141.7;
     int pontos_turisticos = 122;
+    float dens_popu = 2608.87;
+    float per_capita = 9.57
+
+    printf("pib per capita: %f", per_capita);
+    printf("densidade popilacional: %f", dens_popu);
 
     return 0;
 }
