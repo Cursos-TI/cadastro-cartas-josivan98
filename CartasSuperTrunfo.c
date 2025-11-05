89371b1ad60e628db254dee04c425b1b3a982b11
josivan98 <josivanviana16@gmail.com>
2025-11-05 01:14:53 +0000

    super trunfo finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -34,28 +34,45 @@ int main() {
 
     //Área para entrada de dados
     
+  printf("***SUPER TRUNFO RODANDO***\n");
 
-  printf(" A população A01: %u, é maior que a população BO1? %u %d\n",
+  printf(" A população A: (%u), é maior que a população B? (%u) %d\n",
                                                    populaçaoA, populaçaoB,
                                                    (populaçaoA) > (populaçaoB));
 
-  printf(" area A %.2f é maior que area B %.2f %d \n", areaA, areaB,
+  printf(" area A (%.2f) é maior que area B? (%.2f) %d \n", areaA, areaB,
                                                     (areaA) > (areaB) );
                                                
-  printf(" PIB A %.2f é maior que PIB B %.2f %d \n", pibA, pibB,
+  printf(" PIB A (%.2f) é maior que PIB B? (%.2f) %d \n", pibA, pibB,
                                                     (pibA) > (pibB) );
 
-  printf(" pontos turisticos A %d é maior que pontos tur B %d %d \n", pontos_turisticosA , pontos_turisticosB,
+  printf(" pontos turisticos A (%d) é maior que pontos tur B? (%d) %d \n", pontos_turisticosA , pontos_turisticosB,
                                                     (pontos_turisticosA) > (pontos_turisticosB) );
 
-  printf(" A densidade populacional A %.2f é maior que densi popu B %.2f %d \n", dens_popuA, dens_popuB,
+  printf(" A densidade populacional A (%.2f) é maior que densi popu B? (%.2f )%d \n", dens_popuA, dens_popuB,
                                                     (dens_popuA) > (dens_popuB) );
 
-  printf(" Per capita A %.2f é maior que per capita B %.2f %d \n", per_capitaA, per_capitaB,
+  printf(" Per capita A (%.2f) é maior que per capita B? (%.2f) %d \n", per_capitaA, per_capitaB,
                                                     (per_capitaA) > (per_capitaB) );
 
-   printf(" Super poder A %.2f é maior que Super poder B %.2f %d \n", super_poder, super_poderB,
-                                                    (areaA) > (areaB) );
+   printf(" Super poder A (%.2f) é maior que Super poder B? (%.2f) %d \n", super_poder, super_poderB,
+                                                    (super_poder) > (super_poderB) );
+
+  // resutado final do embate
+printf("            ----\n");
+printf("** RESULTADO FINAL **\n");
+
+printf("População: carta A venceu \n");
+printf("Área: carta B venceu \n");
+printf("PIB: carta A venceu.\n");
+printf("Pontos turísticos: carta A venceu.\n");
+printf("Densidade populacional: carta A venceu.\n");
+printf("Renda per capita: carta B venceu\n");
+printf("Super poder: carta A venceu\n"); 
+
+//vencedor do duelo.
+printf("__________\n");
+printf("VENCEDORA: CARTA A (5) a ()2 CARTA B");
 
 
       
